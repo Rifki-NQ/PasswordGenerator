f9f996fd969cb3ec22d5ebd332c4c28a23ec2144
Rifki-NQ <rifqinf950@gmail.com>
2025-11-09 22:04:57 +0700

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def password():
+    upper = ''.join(random.choice(string.ascii_uppercase))
+    lower = ''.join(random.choices(string.ascii_lowercase, k=5))
+    numbers = ''.join(random.choices(string.digits, k=3))
+
+    PASS = upper + lower + numbers
+    return PASS
+
+print(password())
