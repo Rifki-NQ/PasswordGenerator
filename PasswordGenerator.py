34e01ec6c778c219cd1c38607dee43fb4f112516
unknown <rifqingf950@gmail.com>
2025-11-15 21:23:52 +0700

    indexing input system in menu

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -18,6 +18,10 @@ while True:
     print("1. Show last password made\n2. Create new password\n3. Delete last number")
     menu = input("Input: ")
     if menu.isdigit():
-        break
+        menu = int(menu)
+        if menu > 0 and menu < 4:
+            break
+        else:
+            print("Error: Please input the correct index (1, 2, 3)")
     else:
-        print("Error: Plese use the number index to access the menu")
+        print("Error: Plaese use the number index to access the menu")
